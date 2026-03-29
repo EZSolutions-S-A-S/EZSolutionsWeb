6cdc0f65b36e10afb469b5183cbffefaebd3a447
AFTR05 <andrestoro0303@gmail.com>
2026-03-29 17:27:55 -0500

    fix

---
+++ src/data/clients.ts
@@ -0,0 +1,9 @@
+export const CLIENTS = [
+    {
+        id: 1,
+        nameKey: 'clients.company1Name',
+        typeKey: 'clients.company1Type',
+        nameFallback: 'Colchones Posada',
+        typeFallback: 'Colchonera'
+    }
+];
